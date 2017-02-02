a48bb3dc741fc4666e552aa2dbbd07dabdff4723
MinaSuria <mina.suria@effulgentsolutions.com>
2017-02-02 15:16:36 +0800

    Initial Commit

---
+++ GithubEx2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubEx2
+//
+//  Created by Mina Shee on 2/2/17.
+//  Copyright © 2017 Mina Shee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
